fbee7fd89e79ed19d1fd7177094df3ca7ed57c9f
David Neumann <Kazurik@gmail.com>
2015-04-21 15:43:06 -0400

    Added none functional iteration 1 support

---
+++ KnowlComm/ExampleUse.fsx
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+#load "KnowlComm.fs"
+
+open KnowlComm
+
+printfn "Enter address"
+let address = System.Console.ReadLine()
+printfn "Got %s" address
+let comm = KnowlConnector(I1, address, 9696)
+comm.Register.Add (fun () -> printfn "Connected")
+comm.MessageRecevied.Add (fun m -> printfn "%s:%s" m.ChatName m.Message)
+comm.SendMessage "#kazurik/$2be315d399d70eea" "butts"
---
+++ KnowlComm/KnowlComm.fs
@@ -0,0 +1,56 @@
+﻿namespace KnowlComm
+open System.Net.Sockets
+open System.IO
+
+type KnowlVersion = | I1
+
+type ChatMessage = {ChatName:string; Sender:string; Message:string}
+
+type KnowlConnector (version, address, port) = 
+    let client = new TcpClient(address, port)
+    let stream = client.GetStream()
+    let reader = new BinaryReader(stream)
+
+    let eRegister = Event<_>()
+    let eMessageReceived = Event<ChatMessage>()
+
+    let BToStr data len = System.Text.Encoding.ASCII.GetString(data, 0, len)
+
+    let ReadStringB () =                                                                                      
+         let len = int(reader.ReadByte())                                                                                
+         let data = Array.create (len) 0uy                                                                               
+         BToStr data (reader.Read(data, 0, len))                                                                         
+
+    let ReadStringI () =                                                                                      
+         let len = reader.ReadInt32()                                                                                    
+         let data = Array.create (len) 0uy                                                                               
+         BToStr data (reader.Read(data, 0, len))
+    
+    let ReadMessage () = {ChatName=ReadStringB(); Sender=ReadStringB(); Message=ReadStringI()}
+    let StrToBytes (str:string) = System.Text.Encoding.UTF8.GetBytes(str)
+    let IntToBytes (i:int) = if System.BitConverter.IsLittleEndian then System.BitConverter.GetBytes(i) else Array.rev (System.BitConverter.GetBytes(i))
+
+    let HandleMessagesI1 () = 
+        match reader.ReadByte() with
+        | 1uy -> eRegister.Trigger()
+        | 2uy -> eMessageReceived.Trigger(ReadMessage())
+        | _ -> ()
+
+    let HandleMessages = async{
+        match version with
+        | I1 -> HandleMessagesI1()
+        }    
+
+    //Connect
+    do
+        Async.Start HandleMessages
+        match version with
+        | I1 -> stream.WriteByte(1uy);
+
+    member x.Register = eRegister.Publish
+    member x.MessageRecevied = eMessageReceived.Publish
+    member x.SendMessage (chatName:string) (message:string) =
+        let payload = Array.concat ([[|byte 2; byte chatName.Length|]; 
+                                    StrToBytes chatName;  IntToBytes message.Length; 
+                                    StrToBytes message])
+        stream.Write(payload, 0, payload.Length)
